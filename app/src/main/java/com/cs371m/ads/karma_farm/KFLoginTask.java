192aa0d5b2060d40ce7f9e5554ad10d9b0056dd0
Daniel Dao <danielvudao@utexas.edu>
2014-11-16 16:56:43 -0600

    made some changed to login task, need to get string utils library

--- app/src/main/java/com/cs371m/ads/karma_farm/KFLoginTask.java
+++ app/src/main/java/com/cs371m/ads/karma_farm/KFLoginTask.java
@@ -4,12 +4,32 @@ package com.cs371m.ads.karma_farm;
 import android.content.Context;
 import android.os.AsyncTask;
 
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HTTP;
+
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class KFLoginTask extends AsyncTask<Void, Void, Boolean> {
 
     private static final String TAG = "LoginTask";
-
+    private static final String REDDIT_LOGIN_URL = "https://ssl.reddit.com/api/login";
     protected String mUsername;
     private String mPassword;
     protected String mUserError = null;
@@ -29,7 +49,69 @@ public class KFLoginTask extends AsyncTask<Void, Void, Boolean> {
         return doLogin(mUsername, mPassword, mClient, mContext);
     }
 
+    /*
+    *  On success stores session cookie and modhash in your "RedditSettings"
+    *  On failure does not modify "RedditSettings".
+    *  Should be called from a background thread.
+    *
+    *  @return Error message, or null on success
+    * */
+
     private boolean doLogin(String username, String password, HttpClient client, Context context) {
+        String status = "";
+        String userError = "A login error has occurred, please try again.";
+        HttpEntity entity = null;
+        try {
+            //Construct the data that we're going to use
+
+            List<NameValuePair> user_information = new ArrayList<NameValuePair>();
+            user_information.add(new BasicNameValuePair("user", username.toString()));
+            user_information.add(new BasicNameValuePair("password", password.toString()));
+            user_information.add(new BasicNameValuePair("api_type", "json"));
+
+            HttpPost httpPost = new HttpPost(REDDIT_LOGIN_URL);
+            httpPost.setEntity(new UrlEncodedFormEntity(user_information, HTTP.UTF_8));
+
+            HttpParams params = httpPost.getParams();
+                HttpConnectionParams.setConnectionTimeout(params, 45000);
+                HttpConnectionParams.setSoTimeout(params, 45000);
+
+            //Make the HTTP POST
+            HttpResponse response = client.execute(httpPost);
+            status = response.getStatusLine().toString();
+
+            // If the status doesn't return 200
+            if (!status.contains("OK")){
+                throw new HttpException(status);
+            }
+
+            entity = response.getEntity();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(entity.getContent()));
+            String line = in.readLine();
+            in.close();
+            entity.consumeContent();
+
+            // Failed login attempt from what it seems
+           // if (StringUtils.isEmpty(line)){
+           //     throw new HttpException("No content returned from login POST");
+           // }
+
+        } catch (Exception e) {
+            mUserError = userError;
+            if (entity != null) {
+                try {
+                    entity.consumeContent();
+                } catch (Exception e2) {
+                    Log.e(TAG, "entity.consumeContent()", e);
+                }
+
+                Log.e(TAG, "doLogin()", e);
+            }
+
+            return false;
+
+        }
         return true;
     }
 }
