495c6d1a9b9df3462b2916ebdb1e7eae1c062166
Daniel Dao <danielvudao@utexas.edu>
2014-11-22 16:46:38 -0600

    oauth working for a ghetto webview

--- app/src/main/java/com/cs371m/ads/karma_farm/KFLoginTask.java
+++ app/src/main/java/com/cs371m/ads/karma_farm/KFLoginTask.java
@@ -19,40 +19,67 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
 
-public class KFLoginTask extends Activity {
+public class KFLoginTask extends Activity{
+
     OAuth20Service service;
-    public final static String APIKEY = "2Q4Ul-I8YNTSlQ";
-    public static final String APISECRET = "yQpcp5uWSt_Z073GSGieCgqVxg8";
+    public final static String APIKEY = "B9yYaOVuYvxnRA";
+    public static final String APISECRET = "Q7LWINBuc-NmDMYlJDNr5CkW6YU";
     public final static String CALLBACK = "https://oauth.reddit";
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
 
-    public KFLoginTask() {
-
-    }
-
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //setContentView();
+        setContentView(R.layout.activity_login);
 
         sharedPreferences = getPreferences(MODE_PRIVATE);
         editor = sharedPreferences.edit();
-
-        Button button = (Button) findViewById(R.id.action_login);
+        Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new OnClickListener() {
+
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 startAuthentication();
+
             }
         });
 
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                useExistingAuthentication();
+            }
+        });
+    }
+
+    private void useExistingAuthentication(){
+        service = OAuthService.newInstance(new RedditApi(), APIKEY, APISECRET, new OAuth20ServiceCallback() {
+
+            @Override
+            public void onOAuthAccessTokenReceived(OAuth20Token token) {
+                editor.putString("access_token", token.getAccessToken());
+                editor.putString("refresh_token", token.getRefreshToken());
+                editor.commit();
+            }
+
+            @Override
+            public void onAccessTokenRequestFailed(HootResult result) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+        String accessToken = sharedPreferences.getString("access_token", null);
+        String refreshToken = sharedPreferences.getString("refresh_token", null);
+        OAuth20Token existingToken = new OAuth20Token(accessToken, refreshToken);
+        getInfo(existingToken);
     }
 
     private void startAuthentication(){
         service = OAuthService.newInstance(new RedditApi(), APIKEY, APISECRET, new OAuth20ServiceCallback() {
+
             @Override
             public void onOAuthAccessTokenReceived(OAuth20Token token) {
                 editor.putString("access_token", token.getAccessToken());
@@ -62,56 +89,61 @@ public class KFLoginTask extends Activity {
             }
 
             @Override
-            public void onAccessTokenRequestFailed(HootResult hootResult) {
+            public void onAccessTokenRequestFailed(HootResult result) {
+                // TODO Auto-generated method stub
 
             }
         });
-
         service.setApiCallback(CALLBACK);
         service.setScope("identity");
         service.setDuration("permanent");
         getUserVerification();
-
-
     }
 
     private void getUserVerification(){
+
         final WebView webview = (WebView) findViewById(R.id.webview);
         webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient() {
 
-        webview.setWebViewClient(new WebViewClient(){
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url){
-                if (url.startsWith(CALLBACK)){
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+                // Checking for our successful callback
+                if(url.startsWith(CALLBACK)) {
                     webview.setVisibility(View.GONE);
                     service.getOAuthAccessToken(url);
-                }
 
+                }
                 return super.shouldOverrideUrlLoading(view, url);
             }
+
         });
 
         webview.loadUrl(service.getAuthorizeUrl());
     }
 
     private void getInfo(OAuth20Token token){
-        OAuth20Request request = OAuthRequest.newInstance("https://oauth.reddit.com/api/v1/me", token, service, new OnRequestCompleteListener() {
+        OAuth20Request request = OAuthRequest.newInstance("https://oauth.reddit.com/api/v1/me",token, service, new OnRequestCompleteListener() {
+
             @Override
-            public void onSuccess(HootResult hootResult) {
-                Log.v("into", "final on success: " + hootResult.getResponseString());
+            public void onSuccess(HootResult result) {
+                Log.v("into","final on success: "+result.getResponseString());
             }
 
             @Override
-            public void onNewAccessTokenReceived(OAuth20Token oAuth20Token) {
-
+            public void onNewAccessTokenReceived(OAuth20Token token) {
             }
 
             @Override
-            public void onFailure(HootResult hootResult) {
+            public void onFailure(HootResult result) {
+                // TODO Auto-generated method stub
 
             }
         });
-
         request.get();
     }
+
+
+
 }
